b237a8fa1331e9fde144d933c704bd694676e73a
Anerak <tobiastorresmdp@gmail.com>
2020-08-26 00:36:28 -0300

    Both header and list are working together

--- lib/src/pages/list_page.dart
+++ lib/src/pages/list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:todo_app/src/blocs/db_provider.dart';
+import 'package:todo_app/src/blocs/tasks_bloc.dart';
 
 class TodoListPage extends StatefulWidget {
   final String routeName = 'list-page';
@@ -11,31 +13,159 @@ class _TodoListPageState extends State<TodoListPage> {
   String _title = '';
   String _date = '';
   String _time = '';
+
+  TextEditingController _textEditingController = new TextEditingController();
+
+  final _tasksBloc = new TasksBloc();
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    //_tasksBloc.addTask(new TaskModel(id: 10, title: 'Hey', date: '28-07-2200', time: '13:50'));
+    _tasksBloc.getTasks();
     return SafeArea(
       child: Scaffold(
-        body: CustomScrollView(
+          body: CustomScrollView(
+        slivers: [
+          _addTaskHeader(),
+          StreamBuilder(
+            stream: _tasksBloc.taskStream,
+            builder: (BuildContext context, snapshot) {
+              if (!snapshot.hasData)
+                return Center(
+                  child: Text('No pending tasks'),
+                );
+              final List<TaskModel> data = snapshot.data;
+              return SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(data[index].title),
+                      subtitle:
+                          Text("${data[index].date} - ${data[index].time}"),
+                    );
+                  },
+                  childCount: data.length,
+                ),
+              );
+            },
+          )
+        ],
+      )
+          /*CustomScrollView(
           physics: BouncingScrollPhysics(),
           slivers: <Widget>[
-            _addHeader(),
-            SliverList(
-              delegate:
-                  SliverChildBuilderDelegate((BuildContext context, int index) {
-                return Container(
-                  child: Text(index.toString()),
-                );
-              }, childCount: 30),
-            ),
+            _addTaskHeader(),
+            _tasksList(),
           ],
+        ),*/
+          /*StreamBuilder(
+          stream: _tasksBloc.taskStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<List<TaskModel>> snapshot) {
+            if (!snapshot.hasData) return Center(child: Text("No data"));
+            List<TaskModel> data = snapshot.data;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(data[index].title),
+                subtitle: Text(data[index].id.toString()),
+              ),
+            );
+          },
+        ),*/
+          ),
+    );
+  }
+
+  _bar() {
+    return StreamBuilder(
+      builder: (BuildContext context, AsyncSnapshot<List<TaskModel>> snapshot) {
+        if (!snapshot.hasData)
+          return Center(
+            child: Text('No pending tasks!'),
+          );
+        return ListView.builder(
+          itemBuilder: (context, index) => ListTile(),
+        );
+      },
+      stream: _tasksBloc.taskStream,
+    );
+  }
+
+  _foo() {
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        <Widget>[
+          StreamBuilder(
+            stream: _tasksBloc.taskStream,
+            builder: (BuildContext context,
+                AsyncSnapshot<List<TaskModel>> snapshot) {
+              if (!snapshot.hasData)
+                return Center(child: Text('No tasks pending!'));
+              final data = snapshot.data;
+              return SliverToBoxAdapter(
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) => ListTile(
+                    title: Text(data[index].title),
+                    subtitle: Text("ID: ${data[index].id}"),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  _tasksList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) => StreamBuilder(
+          stream: _tasksBloc.taskStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<List<TaskModel>> snapshot) {
+            if (!snapshot.hasData)
+              return Center(
+                child: Text('No tasks pending'),
+              );
+            final List<TaskModel> data = snapshot.data;
+            print(data);
+          },
         ),
       ),
     );
   }
 
-  SliverAppBar _addHeader() {
+  _tasks2List() {
+    StreamBuilder<List<TaskModel>>(
+      stream: _tasksBloc.taskStream,
+      builder: (BuildContext context, AsyncSnapshot<List<TaskModel>> snapshot) {
+        print(_tasksBloc.taskStream);
+        if (!snapshot.hasData || snapshot.data[0] == null)
+          return Center(
+            child: Text("Loading"),
+          );
+        return SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return ListTile();
+            },
+            childCount: snapshot.data.length,
+          ),
+        );
+      },
+    );
+  }
+
+  SliverAppBar _addTaskHeader() {
     return SliverAppBar(
-      expandedHeight: 200.0,
+      expandedHeight: 250.0,
       pinned: true,
       floating: true,
       snap: false,
@@ -50,39 +180,60 @@ class _TodoListPageState extends State<TodoListPage> {
                 height: 30.0,
               ),
               TextField(
+                controller: _textEditingController,
                 keyboardType: TextInputType.text,
                 maxLength: 140,
                 maxLengthEnforced: true,
                 onChanged: (value) => setState(() => _title = value.trim()),
                 decoration: InputDecoration(
+                  hintText: 'Task name',
                   prefixIcon: Icon(Icons.assignment),
                 ),
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  IconButton(
-                    icon: Icon(Icons.date_range),
-                    onPressed: () {
-                      FocusScope.of(context).requestFocus(new FocusNode());
-                      _datePicker(context);
-                    },
+                  InkWell(
+                    onLongPress: () => setState(() => _date = ''),
+                    onTap: () => _datePicker(context),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(Icons.date_range),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(_date),
+                      ],
+                    ),
                   ),
-                  Text(_date),
-                  IconButton(
-                    icon: Icon(Icons.watch_later),
-                    onPressed: () {
-                      FocusScope.of(context).requestFocus(new FocusNode());
-                      _timePicker(context);
-                    },
+                  SizedBox(width: 20.0),
+                  InkWell(
+                    onLongPress: () => setState(() => _time = ''),
+                    onTap: () => _timePicker(context),
+                    child: Row(
+                      children: [
+                        Icon(Icons.access_time),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(_time),
+                      ],
+                    ),
                   ),
-                  Text(_time),
-                  SizedBox(width: 40.0),
-                  IconButton(
-                    icon: Icon(Icons.add_circle),
-                    onPressed: () {},
-                  )
                 ],
               ),
+              SizedBox(
+                height: 20.0,
+              ),
+              InkWell(
+                child: Icon(
+                  Icons.add_circle,
+                  size: 40.0,
+                  color: _title.trim() != '' ? Colors.red : Colors.blue,
+                ),
+                onTap: _title.trim() != '' ? _submitTask : null,
+              )
             ],
           ),
         ),
@@ -114,11 +265,19 @@ class _TodoListPageState extends State<TodoListPage> {
           TimeOfDay(hour: currentTime.hour, minute: currentTime.minute + 5),
       cancelText: '',
     );
-
     if (picked != null) {
       setState(() {
         _time = "${picked.hour}:${picked.minute}";
       });
     }
+  }
+
+  void _submitTask() {
+    setState(() {
+      _textEditingController.clear();
+      _title = '';
+      _date = '';
+      _time = '';
+    });
   }
 }
